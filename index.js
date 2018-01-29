e9efe2a40f4c72688fe23b4f404dc03b56ab84fb
= <=>
2018-01-29 23:33:28 +0000

    Test Route

--- index.js
+++ index.js
@@ -60,5 +60,8 @@ app.get('/webhook', (req, res) => {
     }
   });
 
+  app.get('/', (req,res) => {
+      res.send('Running...')
+  })
 
 app.listen(process.env.PORT || 3000, () => console.log('webhook is listening'));
