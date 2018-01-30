8c272aa119f76fb88c1d94b40743f0c765de3d82
= <=>
2018-01-30 00:06:58 +0000

    Add message hooks

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const
   app = express().use(bodyParser.json())
 
 
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+
 app.post('/webhook', (req, res) => {  
  
     let body = req.body;
@@ -15,11 +17,15 @@ app.post('/webhook', (req, res) => {
   
       // Iterates over each entry - there may be multiple if batched
       body.entry.forEach(function(entry) {
-  
-        // Gets the message. entry.messaging is an array, but 
-        // will only ever contain one message, so we get index 0
+        
+        // Gets the body of the webhook event
         let webhook_event = entry.messaging[0];
         console.log(webhook_event);
+
+        // Get the sender PSID
+        let sender_psid = webhook_event.sender.id;
+        console.log('Sender PSID: ' + sender_psid);
+
       });
   
       // Returns a '200 OK' response to all requests
@@ -62,5 +68,20 @@ app.get('/webhook', (req, res) => {
   app.get('/', (req,res) => {
       res.send('Running...')
   })
+
+    // Handles messages events
+    function handleMessage(sender_psid, received_message) {
+
+    }
+
+    // Handles messaging_postbacks events
+    function handlePostback(sender_psid, received_postback) {
+
+    }
+
+    // Sends response messages via the Send API
+    function callSendAPI(sender_psid, response) {
+    
+    }
 
 app.listen(process.env.PORT || 3000, () => console.log('webhook is listening'));
